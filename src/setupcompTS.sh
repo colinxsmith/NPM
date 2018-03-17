2fc22d181dd33362f7cbd598cbc99bfe108f3dca
Colin Smith <colinxsmith@gmail.com>
2018-03-17 16:32:08 +0000

    This is more usefull

--- src/setupcompTS.sh
+++ src/setupcompTS.sh
@@ -4,29 +4,29 @@ rm index.html app.ts app.css bundle.js
 
 if [ $1 == "bar" ]; then
 	echo Setting up for $1 charts
-	ln -s ~/web/scratch/bar.ts app.ts
-	ln -s ~/web/scratch/bar.css app.css
-	ln -s ~/web/scratch/indexbarTS.html index.html
+	ln -s ../../scratch/bar.ts app.ts
+	ln -s ../../scratch/bar.css app.css
+	ln -s ../../scratch/indexbarTS.html index.html
 elif [ $1 == "pie" ]; then
 	echo Setting up for $1 charts
-	ln -s ~/web/scratch/twoPieCharts.ts app.ts
-	ln -s ~/web/scratch/style.css app.css
-	ln -s ~/web/scratch/index2pTS.html index.html
+	ln -s ../../scratch/twoPieCharts.ts app.ts
+	ln -s ../../scratch/style.css app.css
+	ln -s ../../scratch/index2pTS.html index.html
 elif [ $1 == "squares" ]; then
 	echo Setting up for $1 charts
-	ln -s ~/web/scratch/squares.ts app.ts
-	ln -s ~/web/scratch/squares.css app.css
-	ln -s ~/web/scratch/indexsqTS.html index.html
+	ln -s ../../scratch/squares.ts app.ts
+	ln -s ../../scratch/squares.css app.css
+	ln -s ../../scratch/indexsqTS.html index.html
 elif [ $1 == "gauge" ]; then
 	echo Setting up for $1 charts
-	ln -s ~/web/scratch/gauge.ts app.ts
-	ln -s ~/web/scratch/gauge.css app.css
-	ln -s ~/web/scratch/indexgTS.html index.html
+	ln -s ../../scratch/gauge.ts app.ts
+	ln -s ../../scratch/gauge.css app.css
+	ln -s ../../scratch/indexgTS.html index.html
 elif [ $1 == "panel" ]; then
 	echo Setting up for $1 charts
-	ln -s ~/web/scratch/panel.ts app.ts
-	ln -s ~/web/scratch/panel.css app.css
-	ln -s ~/web/scratch/indexpTS.html index.html
+	ln -s ../../scratch/panel.ts app.ts
+	ln -s ../../scratch/panel.css app.css
+	ln -s ../../scratch/indexpTS.html index.html
 else
 	echo No parameter, not setting up anything!
 fi
